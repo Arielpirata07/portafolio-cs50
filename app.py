c68242fca81de7e28618e41e7eb1ce0789d0588f
Arielpirata07 <pcarielpirata07@hotmail.com>
2026-04-29 19:01:00 -0300

    Fix: Cambiado sistema de sesión a nativo para evitar PermissionError

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ app = Flask(__name__)
 # Agrega esta línea:
 app.secret_key = "una_clave_super_secreta_y_segura"
 
-# Configure session to use filesystem (instead of signed cookies)
+"""# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_FILE_DIR"] = "/tmp"
-Session(app)
+Session(app)"""
 
 # Definimos dónde se guardarán las imágenes
 UPLOAD_FOLDER = 'static/img'
